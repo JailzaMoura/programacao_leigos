a79f0f59b8c6712c38b880e0f7735f23812b5c1b
JailzaMoura <jailzamoura.messias@gmail.com>
2023-09-14 19:20:24 -0300

    Como funciona o computador

--- Secao02-Fundamentos-informática/aula03_o_que_vamos_aprender.py
+++ Secao02-Fundamentos-informática/aula03_o_que_vamos_aprender.py
@@ -1,3 +1,3 @@
 """
-Breve apresentação sobre informática básica e os principais conceitos sobre computador. 
+Breve apresentação sobre informática básica e os principais conceitos sobre computador
 """
---
+++ Secao02-Fundamentos-informática/aula04-como_funciona_um_computador.py
@@ -0,0 +1,32 @@
+"""
+Funcionamento do hardware:
+
+- processador:
+	*tipos: Intel, AMD, AMR
+
+- memória:
+	*tipos: voláteis - memória RAM 4GB ou 8GB
+            Não voláteis - HD, SSD, disco rigido, hard disc
+
+- dispositivos de comunicação:
+	*tipos: rede - cabeada
+		    não cabeada - wireless
+	        bluethoot
+		    USB - pendrive
+
+- dispositivos de saída:
+	*tipos: som - caixa de som
+		    vídeo - monitor
+		    impressora
+
+- dispositivos de entrada:
+	*tipos: teclado, mouse
+
+- sistema operacional:
+	*tipos: Windws
+		Linux - android, mais usado em celulares
+		Mac OS X - Apple
+
+- boot:
+	realiza a verificação do processador, dispositivos de entrada e saída, memórias e SO
+"""
