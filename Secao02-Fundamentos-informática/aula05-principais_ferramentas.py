0c18cc7a2b85c304259cd68852a26659e1225032
JailzaMoura <jailzamoura.messias@gmail.com>
2023-09-18 18:52:35 -0300

    Principais ferramentas do computador

---
+++ Secao02-Fundamentos-informática/aula05-principais_ferramentas.py
@@ -0,0 +1,50 @@
+"""
+Principais ferramentas de informática basica, muito importantes para TI.
+
+- Gerenciador de arquivos: criar arquivos e diretórios; compactar ou descompactar arquivos;
+cortar, copiar e colar.
+
+as pastas, diretórios e arquivos, são a mesma coisa, onde usamos para guardar, colar ou ler os
+arquivos armazenados.
+
+ctrl V = colar
+ctrl C = copiar
+ctrl X = recortar
+
+Compactar arquivos significa criar um ZIP, juntar arquivos ou diretórios em um único arquivo
+compactado, presionado ou de tamanho menor, para facilitar o envio. No arquivo, presionar o botão
+direito e clicar em comprimir ou compactar, clicar em .zip ( mais utilizado).
+
+- Sistemas de arquivo: estrutura de diretórios
+
+No meu computados, é onde fica os diretórios dos usuários.
+
+- Navegador Web: Firefox, Google Chrome, Opera, Safari, Internet Explore.
+
+Temos diversos navegadores e São utilizados para navegar na web. Importante sabermos qual a
+versão que estamos utilizando.
+
+- Painel de controle: utilizado para realizar as configurações do sistema.
+
+Entar no painel de controle, ir em configuraões, teremos diversas configurações no computador,
+temos que deixar sempre atualizado.
+
+- Path do sistema: é uma variavel do sistema, usado pelo SO para localizar executáveis
+importantes.
+
+Quando instalamos um aplicativo ou uma linguagem de programação, o executáveldo software
+deve ser instalado ao Path do Sistema para que ele seja executavél, ou seja, reconhecido.
+
+- Terminal: são todos os sistemas que utilizamos no SO.
+
+Exemplos: Windwos (Prompt de comando). Em um simples comando, podemos criar varios diretórios.
+
+- Editores de texto: temos dois tipos
+
+texto puro não possui formatação, é usado em programação.
+
+rich text é o editor que conseguimos realizar a configuração de texto, não é usado em programação.
+
+- Editores de imagem: devemos conhecer as ferramentas para criar e editar imagens.
+
+"""
