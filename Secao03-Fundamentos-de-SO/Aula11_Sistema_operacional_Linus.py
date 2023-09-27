6ca5a8136cc798c19c0e0394dd946687f2b7bb11
JailzaMoura <jailzamoura.messias@gmail.com>
2023-09-27 19:04:16 -0300

    SO Linux

---
+++ Secao03-Fundamentos-de-SO/Aula11_Sistema_operacional_Linus.py
@@ -0,0 +1,30 @@
+"""
+SO do Linux
+
+No site DistroWatch.com tem-se os 100 melhores distribuidores de versões
+de SO mais atualizadas do Linux, para iniciantes o melhor para usar é o
+Ubuntu ou Linux Mint.
+
+Outro site importante é o TOP500, são listados os 500 maiores super
+computadores do mundo.
+
+o Linux é o mais utilizado mundialmente, pois apresenta gratuidade,
+é estável e possui suporte para diferenciar os Hadwares.
+
+No Linux usamos também o Gerenciador de arquivos, todos os arquivos ou
+diretórios que começam com ., são onde ficam os arquivos ocultos e para
+visualizado presionamos o Crtol H e para esconder os arquivos ocultos,
+precionamos novamente o Crtol H.
+
+O sistema de arquivos do Linux fica em -> outros locais -> computador e
+ira ser apresentado todo o sistema de arquivos e a estrutura de diretório
+do Linux. Muito parecido com o Windows que ja aprendemos.
+
+Navegadores Web - o Linux ja possui na loja de aplicativos no Ubuntu,
+varios navegadores que podemos utilizar e buscar por categoria.
+
+Nos editores de texto podemos realizar a formatação, chamamos de texto
+rico, porem, não é usado em programação, para ser usado em programação
+temos que usar o editor de texto puro.
+
+"""
