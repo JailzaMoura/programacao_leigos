1963adf8180ad0664dc2f36bef2eb917e1ac28b2
JailzaMoura <jailzamoura.messias@gmail.com>
2023-09-27 19:00:17 -0300

    SO Windows

---
+++ Secao03-Fundamentos-de-SO/Aula10_Sistema_operacional_Windows.py
@@ -0,0 +1,52 @@
+"""
+o SO Windows não é recomendado para realizar desenvolvimento, ele é mais
+utilizado para usuários comuns. O lixus e o MAC apresentam maior
+facilidade nesse quisito de desenvolvimento.
+
+Mais para quem não tiver outra opção para desenvolver, vamos estudar
+sobre as principis ferramentas que irão nos ajudar no próprio Windows.
+
+- Gerenciador de arquivos: é conhecido como Windows Explore - que
+esta localizado na barra de ferramentas inferior.
+    * podemos criar nova pasta ou diretórios e esta localizado em:
+    Este computador;
+    * podemos criar editores de texto, sendo texto puro - escrevemos
+    programas e não pode ser configurado, ja no Wit text - podemos
+    formatar nosso texto;
+    * utilizamos todas as formatações de copiar (Ctrl C), colar
+    (Ctrl V), recortar (Ctrl X) ou excluir, nos diretórios que fazemos;
+    * criamos também arquivos compactados, botão direito - enviar para
+    - pasta compactada;
+
+- O SO do Windows esta localizado no Disco local(C:)
+    * temos diretorio de : Arquivos de Programas (SO que tenha 64 bytes)
+    ,Arquivos de Programa (x86) (SO que tenha 32 bytes), PerfLogs
+    (preferencias e Logs), Usuários(dentro de cada diretório é criado
+    um arquivo com o nome de quem utiliza), Windows;
+
+- Navegador Web:
+   * existem diversos: Fire Fox, Google Chrome, Safira, ópera. No
+   Sistema Windows utilizamos o Edge, antigamente chamado de Windows
+   explore;
+
+- Painel de controle
+   * menu iniciar e ir para configurações, onde fica todas as
+   configurações que precisamos fazer, por exemplos: nos sistemas,
+   dispositivos, contas, hora e idioma etc;
+   * podemos utilizar o Path do sistema, quando instalamos um
+   Software, ele coloca o executável disponivel no sistema, e esse
+   local é chamado de Path do sistema;
+
+- Terminal
+   * o terminal no Windows chamamos de Prompt de Comando:
+   botão iniciar, digita CMD, ja ira aparecer o Prompt de comando,
+   ou seja, tela preta onde podemos navegar no sistema.
+
+- Editores de texto
+   * O Windows nos fornece apenas o blocos de notas, para editar texto
+   puro e programas em Java, Python. HTML ou Java scrip.
+
+- Editores de imagens
+   * o Windows ja vem com o Paint, abrir uma imagem ou fazer edição.
+
+"""
