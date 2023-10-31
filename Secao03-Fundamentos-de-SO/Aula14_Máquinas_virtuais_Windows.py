9114321aa9d0d9fa97673fb36cc277dc94a63849
JailzaMoura <jailzamoura.messias@gmail.com>
2023-10-31 19:05:20 -0300

    Maquinas Virtuais Windows

---
+++ Secao03-Fundamentos-de-SO/Aula14_Máquinas_virtuais_Windows.py
@@ -0,0 +1,21 @@
+"""
+Maquinas virtuais Windows
+
+As máquinas virtuais são computadores de software com a mesma
+funcionalidade que os computadores físicos. Assim como os computadores
+físicos, elas executam aplicativos e um sistema operacional.
+
+
+Qual é a máquina virtual do Windows?
+Entre elas estão Hyper-V (Windows), VMware (Windows), Oracle VirtualBox
+(Windows, macOS e Linux) e Parallels (macOS). Esses são alguns dos
+principais programas de virtualização.
+
+Aprendemos como instalar e configurar a VM do Windows 10. Atraves do
+VirtualBox -> Oracle -> Extension Pack.
+
+Qualquer Software que queremos desintalar, ir em: painal de controle ->
+programas -> desinstalar um programas -> clicar no software e em
+desinstalar.
+
+"""
