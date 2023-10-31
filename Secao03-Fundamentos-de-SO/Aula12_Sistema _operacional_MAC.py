e601d7e806a392dfe95b4ca5274d7cf1d641d981
JailzaMoura <jailzamoura.messias@gmail.com>
2023-10-31 19:00:28 -0300

    SO do MAC

---
+++ Secao03-Fundamentos-de-SO/Aula12_Sistema _operacional_MAC.py
@@ -0,0 +1,39 @@
+"""
+SO do MAC
+
+O gerenciador de arquivos do MAC, se chama FAINDER, ele gerencia arquivos
+e diretórios, pode ser criado novos diretórios e organizados por ordem
+alfabética.
+
+O navegador de internet padrão do MAC é o Safari (padrão da apple), é
+utilizado para acessar páginas da internet. Porem podemos instalar outros
+navegadores de internet,como por exemplo: google, chrome e opera.
+
+Podemos criar arquivos ocultos, mais o MAC não ira mostrar nos diretório.
+Para visualizar esses arquivos, devemos clicar em: ctrl + shift + . Os
+arquivos que começam com ., são os arquivos ocultos do MAC, geralmente são
+arquivos de configuração do sistema.
+
+Para renomear um arquivo ou diretório, clicamos no nome do arquivo, deixar
+selecionado, presionamos o enter, modificamos o nome e clica em enter
+novamente.
+
+Usamos: Ctrl C = copiar
+        Ctrl V = colar
+        Ctrl + opsion = mover
+        comand S = salvar
+
+O Click duplo no arquivo, serve para descompactar o arquivo.
+
+O terminal é importante em qualquer SO, importante deixar o terminal
+amostra, para facilitar o trabalho do desenvolvedor. Dentro do terminal,
+podemos executar muitas ooções de comando.
+
+A loja de aplicativos da Apple é a App Store, da para fazer buscas de
+aplicativos ou selecionar por busca de categorias.
+
+Importante saber no MAC: manipular arquivos e diretórios, compactar e
+descompactar arquivos, criar e excluir diretórios, criar e excluir
+arquivos e adicionar binarios no beef do sistema.
+
+"""
