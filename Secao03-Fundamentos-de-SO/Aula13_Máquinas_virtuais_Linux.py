d76a17a8deeb2f35fa619b4eacc47f5ed4f3f73d
JailzaMoura <jailzamoura.messias@gmail.com>
2023-10-31 19:03:03 -0300

    Maquinas virtuais Linux

---
+++ Secao03-Fundamentos-de-SO/Aula13_Máquinas_virtuais_Linux.py
@@ -0,0 +1,15 @@
+"""
+Maquinas virtuais Linux
+
+Uma máquina virtual (VM) é um ambiente virtual que funciona como uma
+simulação de um computador, com sua própria CPU, memória, interface
+de rede e armazenamento. Os recursos do sistema são separados do
+computador que o está simulando e são provisionados adequadamente para
+que a máquina virtual (VM) possa utilizá-los.
+
+As duas principais maquinas virtuais são: VirtualBox e VmWare.
+
+Aprendemos como instalar o Software da MV, suas configurações e a
+desinstalação.
+
+"""
