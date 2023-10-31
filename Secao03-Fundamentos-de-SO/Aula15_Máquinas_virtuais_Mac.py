a841017bbcda325091d2b645174ff40780d78d3c
JailzaMoura <jailzamoura.messias@gmail.com>
2023-10-31 19:06:46 -0300

    Maquinas Virtuais MAC

---
+++ Secao03-Fundamentos-de-SO/Aula15_Máquinas_virtuais_Mac.py
@@ -0,0 +1,15 @@
+""""
+Maquinas Virtuais MAC
+
+No MAC usamos o Virtual Box, é gratuito e a ultima versão 6,0. Devemos baixar
+a versão para cada tipo de SO que estivermos usando.
+
+Aprendemos como fazer a instalação do VirtualBOX no computador. E instalamos
+também o Extension Pack-6.0, permite que as máquinas virtuais tenham mais
+recurso.
+
+As máquinas virtuais ficam locadas no diretório VM, criamos a do Ubuntu
+e Windows 10. Para excluir uma máquina virtual, clicar com botão direito e
+REMOVE.
+
+"""
